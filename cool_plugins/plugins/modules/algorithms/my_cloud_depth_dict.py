7512783f1a79082a9db13d0bc6a0fd22337c445e
mindyls <mindyls@users.noreply.github.com>
2024-08-08 02:03:31 +0000

    Adding docstring to my_cloud_depth_dict.py
    Ensure flake8 passes

--- cool_plugins/plugins/modules/algorithms/my_cloud_depth_dict.py
+++ cool_plugins/plugins/modules/algorithms/my_cloud_depth_dict.py
@@ -24,6 +24,46 @@ def call(
     norm=False,
     inverse=False,
 ):
+    """My cloud depth product algorithm manipulation steps.
+
+    This algorithm expects Cloud Height Acha and Base in that order.
+
+    Parameters
+    ----------
+    xarray_dict: dict of xarray Dataset
+        * xarray datasets containing variables "variables" of channel data
+        * Channel data should be in degrees Kelvin
+    variables: list of str
+        * List of variables that will be used out of xobj within this algorithm.
+        * These variables are retrieved from the list of variables in the product
+          plugin.
+    product_name: str
+        * Name of the product that is being produced.
+        * Retrieved from the product plugin.
+        * This will be the variable name of the final manipulated dataset
+          that will be added to the return xarray Dataset.
+    output_data_range: list of float
+        * List containing minimum and maximum value for final output data.
+    scale_factor: float
+        * The scale to multply your data to get it in the correct output range.
+    min_outbounds: str, default="crop"
+        * Operation to perform for data below minimum value
+        * crop or mask
+    max_outbounds: str, default="mask"
+        * Operation to perform for data above maximum value
+        * crop or mask
+    norm: bool, default=False
+        * Specify whether to normalize the data or not.
+    inverse: bool, default=False
+        * Specify whether to invert the data or not.
+
+    Returns
+    -------
+    xarray Dataset
+        * xarray Dataset containing the original variables, plus variable
+          "product_name" of appropriately scaled channel data
+        * degrees Kelvin.
+    """
 
     # DATA:cloud_height_acha
     cth_dsname, cth_varname = variables[0].split(":")
